0caff1cffa3cb6cfece9d0408a9bb3686d2a25d9
cwojtczak <craigwojtczak@gmail.com>
2025-03-11 22:15:01 -0500

    initial commit on craig branch

--- src/components/CardSearch.tsx
+++ src/components/CardSearch.tsx
@@ -7,6 +7,35 @@ function CardSearch() {
   return (
     <div className='cardSearch'>
         Search and Results
+
+    <table>
+      <tr>
+        <th></th>
+        <th>Set</th>
+        <th>Card Name</th>
+        <th>Raw Value</th>
+        <th>PSA 8 Value</th>
+        <th>PSA 9 Value</th>
+        <th>PSA 10 Value</th>
+        <th>PSA 8 Profit</th>
+        <th>PSA 9 Profit</th>
+        <th>PSA 10 Profit</th>   
+      </tr>
+      <tr>
+        <td>
+         <img src="src\assets\Alakazam_1.jpg" alt="Alakazam 1" width='75em'></img>
+        </td>
+        <td>151</td>
+        <td>Alakazam EX #201</td>
+        <td>$67.74</td>
+        <td>$68.75</td>
+        <td>$70.53</td>
+        <td>$80.53</td>
+        <td>$14.38</td>
+        <td>$47.68</td>
+        <td>$143.61</td>
+      </tr>
+    </table>
     </div>
   )
 }
--- src/components/TilePSA.tsx
+++ src/components/TilePSA.tsx
@@ -6,24 +6,7 @@ function TilePSA() {
 
   return (
     <div className='psa'>
-      Current Price Here
-      <div className='psa-tile'>
-        <h3>Loss</h3>
-        <br></br>
-        <b>$13.42</b>
-        <p className='small-text underline-text'>$59.87</p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <ul className='tile-categories'>
-          <li>Month</li>
-          <li>Pop.</li>
-          <li>Volatility</li>
-          <li>Liquidity</li>
-          </ul>
-      </div>
+      Back to the drawing board
     </div>
   )
 }
