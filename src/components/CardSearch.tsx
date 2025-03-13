85f8a7b0331535ad6469c6f2fef78b4cc1e0348f
Maxwell Paradis <maxwell@paradisbend.com>
2025-03-13 17:09:11 -0500

    feat: iterative card search mockup

--- src/components/CardSearch.tsx
+++ src/components/CardSearch.tsx
@@ -4,6 +4,21 @@ import '../App.css'
 function CardSearch() {
   //const [count, setCount] = useState(0)
 
+  const defCard = {
+    id: 1,
+    set: 151,
+    name: 'Alakazam EX #201',
+    value: 67.74,
+    v8: 68.75,
+    v9: 70.53,
+    v10: 80.53,
+    p8: 14.38,
+    p9: 47.68,
+    p10: 143.61
+  };
+
+  const cardSelection = [defCard, defCard, defCard, defCard, defCard, defCard]
+
   return (
     <div className='cardSearch'>
         Search and Results
@@ -21,20 +36,22 @@ function CardSearch() {
         <th>PSA 9 Profit</th>
         <th>PSA 10 Profit</th>   
       </tr>
-      <tr>
+      {cardSelection.length > 0 ? cardSelection.map((x:any, i:number) =>
+        <tr key={i} >
         <td>
          <img src="src\assets\Alakazam_1.jpg" alt="Alakazam 1" width='75em'></img>
         </td>
-        <td>151</td>
-        <td>Alakazam EX #201</td>
-        <td>$67.74</td>
-        <td>$68.75</td>
-        <td>$70.53</td>
-        <td>$80.53</td>
-        <td>$14.38</td>
-        <td>$47.68</td>
-        <td>$143.61</td>
+        <td>{x.set}</td>
+        <td>{x.name}</td>
+        <td>${x.value}</td>
+        <td>${x.v8}</td>
+        <td>${x.v9}</td>
+        <td>${x.v10}</td>
+        <td>${x.p8}</td>
+        <td>${x.p9}</td>
+        <td>${x.p10}</td>
       </tr>
+      ) : null}
     </table>
     </div>
   )
