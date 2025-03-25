c20ee61f02df127fe5953bac4251663679165c13
cwojtczak <craigwojtczak@gmail.com>
2025-03-24 22:07:27 -0500

    Worked on PSA tile

--- src/components/TilePSA.tsx
+++ src/components/TilePSA.tsx
@@ -6,20 +6,41 @@ function TilePSA() {
 
   return (
     <div className='psa'>
+        <table className='card-grid'>
+            <th>PSA 7</th>
+            <th>Loss</th>
+        </table>
+
       <div className='card'>
         <h3>Loss</h3>
         <h2>$13.42</h2>
         <h5>$59.95</h5>
       </div>
+
       <div className='card'>
         <h3>Loss</h3>
         <h2>$13.42</h2>
         <h5>$59.95</h5>
-      </div>
-      <div className='card'>
-        <h3>Loss</h3>
-        <h2>$13.42</h2>
-        <h5>$59.95</h5>
+        <table className='card-table'>
+          <tr>
+            <th><img src='src\assets\stock up.png' width="30"></img></th>
+            <th><img src='src\assets\progress bar.png' width="40"></img></th>
+            <th><img src='src\assets\progress bar.png' width="40"></img></th>
+            <th><img src='src\assets\progress bar.png' width="40"></img></th>
+          </tr>
+          <tr>
+            <td>+3.4%</td>
+            <td>Low</td>
+            <td>High</td>
+            <td>High</td>
+          </tr>
+          <tr>
+            <th>Month</th>
+            <th>Pop.</th>
+            <th>Volatility</th>
+            <th>Liquidiy</th>
+          </tr>
+        </table>
       </div>
       <div className='card'>
         <h3>Loss</h3>
