66ab914506d2050a9218e20bfdc7c564562d176c
cwojtczak <craigwojtczak@gmail.com>
2025-04-01 22:16:44 -0500

    Card Layout Updates

--- src/components/TilePSA.tsx
+++ src/components/TilePSA.tsx
@@ -6,97 +6,86 @@ function TilePSA() {
 
   return (
     <div className='psa'>
-      <div><img src='src\assets\Left Arrow.svg' height="30"></img></div>
-      <div className='card'>
-        <h3>Loss</h3>
-        <h2>$13.42</h2>
-        <h5>$59.95</h5>
-        <table className='card-table'>
-          <tr>
-            <th><img src='src\assets\stock up.png' width="30"></img></th>
-            <th><img src='src\assets\Progress Bar_Red.svg' width="40"></img></th>
-            <th><img src='src\assets\Progress Bar_Red.svg' width="40"></img></th>
-            <th><img src='src\assets\Progress Bar_Red.svg' width="40"></img></th>
-          </tr>
-          <tr>
-            <td>+3.4%</td>
-            <td>Low</td>
-            <td>High</td>
-            <td>High</td>
-          </tr>
-          <tr>
-            <th>Month</th>
-            <th>Population</th>
-            <th>Volatility</th>
-            <th>Liquidiy</th>
-          </tr>
-        </table>
+      <div className='card-container'>
+        <div className='card-grid item-1'>PSA</div>
+        <div className='card-grid item-2'>7</div>
+        <div className='card-grid item-3'>Profit</div>
+        <div className='card-grid item-4'>$13.42</div>
+        <div className='card-grid item-5'>$59.95</div>
+        <div className='card-grid item-6'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-7'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-8'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-9'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-10'>+3.4%</div>
+        <div className='card-grid item-11'>Low</div>
+        <div className='card-grid item-12'>High</div>
+        <div className='card-grid item-13'>High</div>
+        <div className='card-grid item-14'>Month</div>
+        <div className='card-grid item-15'>Population</div>
+        <div className='card-grid item-16'>Volatility</div>
+        <div className='card-grid item-17'>Liquidity</div>
       </div>
       
 
-      <div className='card'>
-        <h3>Loss</h3>
-        <h2>$13.42</h2>
-        <h5>$59.95</h5>
-        <table className='card-table'>
-          <tr>
-            <th><img src='src\assets\stock up.png' width="30"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-          </tr>
-          <tr>
-            <td>+3.4%</td>
-            <td>Low</td>
-            <td>High</td>
-            <td>High</td>
-          </tr>
-          <tr>
-            <th>Month</th>
-            <th>Population</th>
-            <th>Volatility</th>
-            <th>Liquidiy</th>
-          </tr>
-        </table>
-      </div>
-      
-      <div className='card'>
-        <h3>Loss</h3>
-        <h2>$13.42</h2>
-        <h5>$59.95</h5>
-        <table className='card-table'>
-          <tr>
-            <th><img src='src\assets\stock up.png' width="30"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-            <th><img src='src\assets\progress bar.png' width="40"></img></th>
-          </tr>
-          <tr>
-            <td>+3.4%</td>
-            <td>Low</td>
-            <td>High</td>
-            <td>High</td>
-          </tr>
-          <tr>
-            <th>Month</th>
-            <th>Population</th>
-            <th>Volatility</th>
-            <th>Liquidiy</th>
-          </tr>
-        </table>
+      <div className='card-container'>
+        <div className='card-grid item-1'>PSA</div>
+        <div className='card-grid item-2'>8</div>
+        <div className='card-grid item-3'>Profit</div>
+        <div className='card-grid item-4'>$13.42</div>
+        <div className='card-grid item-5'>$59.95</div>
+        <div className='card-grid item-6'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-7'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-8'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-9'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-10'>+3.4%</div>
+        <div className='card-grid item-11'>Low</div>
+        <div className='card-grid item-12'>High</div>
+        <div className='card-grid item-13'>High</div>
+        <div className='card-grid item-14'>Month</div>
+        <div className='card-grid item-15'>Population</div>
+        <div className='card-grid item-16'>Volatility</div>
+        <div className='card-grid item-17'>Liquidity</div>
       </div>
       
       <div className='card-container'>
-        <div className='card-grid'><h2>PSA 7</h2></div>
-        <div className='card-grid'><h3>Loss</h3></div>
-        <div className='card-grid'><h2>$13.42</h2></div>
-        <div className='card-grid'><h5>$59.95</h5></div>
-        <div className='card-grid'><h5>$59.95</h5></div>
-        <div className='card-grid'><h5>$59.95</h5></div>
-        <div className='card-grid'><h5>$59.95</h5></div>
-        <div className='card-grid'><h5>$59.95</h5></div>
+        <div className='card-grid item-1'>PSA</div>
+        <div className='card-grid item-2'>9</div>
+        <div className='card-grid item-3'>Profit</div>
+        <div className='card-grid item-4'>$13.42</div>
+        <div className='card-grid item-5'>$59.95</div>
+        <div className='card-grid item-6'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-7'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-8'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-9'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-10'>+3.4%</div>
+        <div className='card-grid item-11'>Low</div>
+        <div className='card-grid item-12'>High</div>
+        <div className='card-grid item-13'>High</div>
+        <div className='card-grid item-14'>Month</div>
+        <div className='card-grid item-15'>Population</div>
+        <div className='card-grid item-16'>Volatility</div>
+        <div className='card-grid item-17'>Liquidity</div>
       </div>
-      <div><img src='src\assets\Right Arrow.svg' height="30"></img></div>
+      
+      <div className='card-container'>
+        <div className='card-grid item-1'>PSA</div>
+        <div className='card-grid item-2'>10</div>
+        <div className='card-grid item-3'>Profit</div>
+        <div className='card-grid item-4'>$13.42</div>
+        <div className='card-grid item-5'>$59.95</div>
+        <div className='card-grid item-6'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-7'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-8'><img src='src\assets\Progress Bar_Red.svg' width="30"></img></div>
+        <div className='card-grid item-9'><img src='src\assets\stock up.png' width="30"></img></div>
+        <div className='card-grid item-10'>+3.4%</div>
+        <div className='card-grid item-11'>Low</div>
+        <div className='card-grid item-12'>High</div>
+        <div className='card-grid item-13'>High</div>
+        <div className='card-grid item-14'>Month</div>
+        <div className='card-grid item-15'>Population</div>
+        <div className='card-grid item-16'>Volatility</div>
+        <div className='card-grid item-17'>Liquidity</div>
+      </div>
     </div>
   )
 }
