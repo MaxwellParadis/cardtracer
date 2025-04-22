f248fe9da5490f45f751a59d2114e626b5579f39
cwojtczak <craigwojtczak@gmail.com>
2025-04-21 22:06:56 -0500

    Search filters

--- src/components/CardSearch.tsx
+++ src/components/CardSearch.tsx
@@ -24,22 +24,38 @@ function CardSearch() {
       <input type="text" placeholder="Search..."></input><button>Search</button>
       <br></br>
       <ul className='filter-checkbox-container'>
-        <input type="checkbox" id="Language1" name="Language1" value="English"></input>
-        <label htmlFor="Language1">English</label>
-        <input type="checkbox" id="Language2" name="Language2" value="Japanese"></input>
-        <label htmlFor="Language2">Japanese</label>
-        <input type="checkbox" id="Product1" name="Product1" value="Cards"></input>
-        <label htmlFor="Product1">Cards</label>
-        <input type="checkbox" id="Product2" name="Product2" value="Sealed"></input>
-        <label htmlFor="Product2">Sealed</label>
-        <input type="checkbox" id="Binder" name="Binder" value="Binder"></input>
-        <label htmlFor="Binder">Binder</label>
-        <input type="checkbox" id="PSA8" name="PSA8" value="PSA8"></input>
-        <label htmlFor="PSA8">PSA 8 Profit</label>
-        <input type="checkbox" id="PSA9" name="PSA9" value="PSA9"></input>
-        <label htmlFor="PSA9">PSA 9 Profit</label>
-        <input type="checkbox" id="PSA10" name="PSA10" value="PSA10"></input>
-        <label htmlFor="PSA10">PSA 10 Profit</label>
+        <div>
+          <input type="checkbox" id="Language1" name="Language1" value="English"></input>
+          <label htmlFor="Language1">English</label>
+        </div>
+        <div>
+          <input type="checkbox" id="Language2" name="Language2" value="Japanese"></input>
+          <label htmlFor="Language2">Japanese</label>
+          </div>
+        <div>
+          <input type="checkbox" id="Product1" name="Product1" value="Cards"></input>
+          <label htmlFor="Product1">Cards</label>
+        </div>
+        <div>
+          <input type="checkbox" id="Product2" name="Product2" value="Sealed"></input>
+          <label htmlFor="Product2">Sealed</label>
+        </div>
+        <div>
+          <input type="checkbox" id="Binder" name="Binder" value="Binder"></input>
+          <label htmlFor="Binder">Binder</label>
+        </div>
+        <div>
+          <input type="checkbox" id="PSA8" name="PSA8" value="PSA8"></input>
+          <label htmlFor="PSA8">PSA 8 Profit</label>
+        </div>
+        <div>
+          <input type="checkbox" id="PSA9" name="PSA9" value="PSA9"></input>
+          <label htmlFor="PSA9">PSA 9 Profit</label>
+        </div>
+        <div>
+          <input type="checkbox" id="PSA10" name="PSA10" value="PSA10"></input>
+         <label htmlFor="PSA10">PSA 10 Profit</label>
+        </div>
       </ul>
       <table>
         <tbody>
